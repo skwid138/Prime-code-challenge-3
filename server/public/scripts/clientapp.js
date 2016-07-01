6455b9527577cc6c5f3b09467eb36c03dce5c925
Antoinette Smith <antoinette@primeacademy.io>
2016-07-01 11:11:42 -0500

    Added all necessary AJAX calls

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
   var $treats = $('#treat-display');
   console.log($treats);
 
+  /**---------- AJAX Functions ----------**/
+
   // GET /treats
   function getTreats() {
     $ajax({
@@ -9,6 +11,8 @@ $(document).ready(function () {
       url: '/treats',
     })
     .done(function (treatArray) {
+      console.log('GET /treats returned ', treatArray);
+
       $.each(treatArray, function (index, treat) {
         appendTreat(treat);
       });
@@ -16,13 +20,61 @@ $(document).ready(function () {
   }
 
   // GET /treats?q=thing
+  function searchTreats(query) {
+    $ajax({
+      method: 'GET',
+      url: '/treats?q=' + query,
+    })
+    .done(function (treatArray) {
+      console.log('GET /treats?q=', query, 'returned ', treatArray);
+
+      $.each(treatArray, function (index, treat) {
+        appendTreat(treat);
+      });
+    });
+  }
 
   // POST /treats
+  function postTreat(treat) {
+    $ajax({
+      method: 'POST',
+      url: '/treats',
+      data: treat,
+    })
+    .done(function () {
+      console.log('POST /treats sent ', treat);
+
+      getTreats();
+    });
+  }
 
   // PUT /treats/1
+  function putTreat(treatId, treat) {
+    $ajax({
+      method: 'PUT',
+      url: '/treats/' + treatId,
+      data: treat,
+    })
+    .done(function () {
+      console.log('PUT /treats/', treatId);
+
+      getTreats();
+    });
+  }
 
   // DELETE /treats/1
+  function deleteTreat(treatId) {
+    $ajax({
+      method: 'DELETE',
+      url: '/treats/' + treatId,
+    })
+    .done(function () {
+      console.log('DELETE /treats/', treatId);
 
+      getTreats();
+    });
+  }
+  /** ---------- DOM Functions ----------**/
   function appendTreat(treat) {
 
     // treat-display -> treat row -> treat
